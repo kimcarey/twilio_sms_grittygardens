570775d73412e501810f8e0f10f052abb4063dcf
Kim Carey <kthomas06@gmail.com>
2020-06-13 14:14:04 -0700

    Updated to include route for Twilio API

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,40 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import csv, json
+from twilio.twiml.messaging_response import MessagingResponse
+from dotenv import load_dotenv
+load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/data')
 def get_state_data():
     csvFilePath = open('./data/mydata.csv', 'r')
-    # jsonFilePath = open('mydata.json', 'w')
 
     field_names = ("State", "County", "Grocery stores", "Supercenters & club stores", "Convenience stores",
                    "Specialized food stores", "SNAP-authorized stores", "WIC-authorized stores")
 
     reader = csv.DictReader(csvFilePath, field_names)
+    csv.re
     out = []
     for row in reader:
-        # json.dump(row, jsonFilePath)
-        # jsonFilePath.write('\n')
-        out.append(row)
+       out.append(row)
     return json.dumps(out)
+
+
+@app.route("/sms", methods=["GET", "POST"])
+def sms_reply():
+    '''Respond to incoming text with simple text message'''
+    resp = MessagingResponse()
+
+    resp.message("Your Gritty Garden, coming soon.")
+
+    return str(resp)
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
